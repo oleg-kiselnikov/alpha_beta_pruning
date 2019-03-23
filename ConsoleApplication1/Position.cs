e6658f920a8a48065e6a5ca78c62464f1461aa81
yustina.ivanova <oleg.kiselnikov@yandex.ru>
2019-03-24 00:31:39 +0300

    code

---
+++ ConsoleApplication1/Position.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public struct Position
+    {
+        public int Row;
+
+        public int Col;
+    }
+}
